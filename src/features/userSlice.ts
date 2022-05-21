605232777618b76ffb34ab8d8ee30049b5599767
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-05-22 06:58:23 +0900

    feat: 🎸 user redux

---
+++ src/features/userSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from "../app/store";
+
+interface UserState {
+    uid: string,
+    photoUrl: string,
+    displayName: string
+}
+
+interface UserInitialState {
+    user: UserState
+}
+
+const initialState: UserInitialState = {
+    user: {
+        uid: "",
+        photoUrl: "",
+        displayName: ""
+    }
+}
+
+export const userSlice = createSlice({
+    name: "user",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload
+        },
+        logout: (state) => {
+            state.user = initialState.user
+        }
+    }
+})
+
+export const { login, logout } = userSlice.actions
+
+export const selectUser = (state: RootState) => state.user.user
+
+export default userSlice.reducer
