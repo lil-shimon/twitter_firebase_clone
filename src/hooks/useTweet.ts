02a3558b540724e6457b0cd12448eff05df811a3
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-05-28 21:02:08 +0900

    feat: 🎸 added useTweet

---
+++ src/hooks/useTweet.ts
@@ -0,0 +1,75 @@
+import { ChangeEvent, FormEvent, useCallback, useState } from "react";
+import { db, storage } from "../firebase";
+import firebase from "firebase/app";
+import { useSelector } from "react-redux";
+import { selectUser } from "../slicers/userSlice";
+
+export const useTweet = () => {
+
+    const user = useSelector(selectUser)
+    const [tweetImg, setTweetImg] = useState<File | null>(null)
+    const [tweetMsg, setTweetMsg] = useState('')
+
+    const handleChangeTweetImg = (e: ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files![0]) {
+            setTweetImg(e.target.files![0])
+            e.target.value = ""
+        }
+    }
+
+    const handleChangeTweetMsg = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+        setTweetMsg(e.target.value)
+    }, [tweetMsg])
+
+    // TODO: refactor
+    const sendTweet = useCallback((e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (tweetImg) {
+            const S = "abcdefghijklmnopqrstuvwxyz0123456789"
+            const N = 16
+            const randomChar = Array.from(crypto.getRandomValues(new Uint32Array(N)))
+                .map((n) => S[n % S.length])
+                .join("")
+            const fileName = randomChar + "_" + tweetImg.name
+            const uploadTweetImg = storage.ref(`images/${fileName}`).put(tweetImg)
+            uploadTweetImg.on(
+                firebase.storage.TaskEvent.STATE_CHANGED,
+                () => {
+                },
+                (err) => {
+                    alert(err.message)
+                }, async () => {
+                    await storage.ref("images").child(fileName).getDownloadURL().then(
+                        async (url) => {
+                            await db.collection("posts").add({
+                                avatar: user.photoUrl,
+                                image: url,
+                                text: tweetMsg,
+                                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                                username: user.displayName
+                            })
+                        }
+                    )
+                }
+            )
+        } else {
+            db.collection("posts").add({
+                avatar: user.photoUrl,
+                image: "",
+                text: tweetMsg,
+                timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+                username: user.displayName
+            })
+        }
+        setTweetMsg('')
+        setTweetImg(null)
+    }, [tweetMsg, tweetImg, user])
+
+    return {
+        tweetImg,
+        tweetMsg,
+        handleChangeTweetImg,
+        handleChangeTweetMsg,
+        sendTweet
+    }
+}
