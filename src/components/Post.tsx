67d353a20faa9ea8432aa2d828a4babc2654f62c
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-05-29 13:49:05 +0900

    feat: 🎸 done

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,9 @@
 import styles from './Post.module.css'
 import { FC, FormEvent } from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, makeStyles } from "@material-ui/core";
 import { useComment } from "../hooks/useComment";
 import SendIcon from "@material-ui/icons/Send";
+import { MessageOutlined } from "@material-ui/icons";
 
 interface Props {
     postId: string
@@ -13,8 +14,32 @@ interface Props {
     username: string
 }
 
+export interface Comment {
+    id: string
+    avatar: string
+    text: string
+    timestamp: any
+    username: string
+}
+
+const useStyles = makeStyles((theme) => ({
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+        marginRight: theme.spacing(1),
+    }
+}))
+
 export const Post: FC<Props> = ({ postId, avatar, image, text, timestamp, username }) => {
-    const { comment, handleChangeComment, newComment } = useComment(postId)
+    const classes = useStyles()
+    const {
+        comment,
+        comments,
+        handleChangeComment,
+        newComment,
+        handleChangeOpenComments,
+        openComments
+    } = useComment(postId)
     return (
         <div className={styles.post}>
             <div className={styles.post_avatar}>
@@ -37,24 +62,45 @@ export const Post: FC<Props> = ({ postId, avatar, image, text, timestamp, userna
                         <img src={image} alt={"tweet"}/>
                     </div>
                 )}
-                <form onSubmit={newComment}>
-                    <div className={styles.post_form}>
-                        <input
-                            className={styles.post_input}
-                            type={"text"}
-                            placeholder={"Type new comment..."}
-                            value={comment}
-                            onChange={(e) => handleChangeComment(e)}
-                        />
-                        <button
-                            className={comment ? styles.post_button : styles.post_buttonDisable}
-                            disabled={!comment}
-                            type={"submit"}
-                        >
-                            <SendIcon className={styles.post_sendIcon}/>
-                        </button>
-                    </div>
-                </form>
+
+                <MessageOutlined
+                    className={styles.post_commentIcon}
+                    onClick={handleChangeOpenComments}
+                />
+
+                {openComments &&
+                    <>
+                        {comments.map((comment) => (
+                            <div key={comment.id} className={styles.post_comment}>
+                                <Avatar src={comment.avatar} className={classes.small}/>
+                                <span className={styles.post_commentUser}>@{comment.username}</span>
+                                <span className={styles.post_commentText}>{comment.text}</span>
+                                <span className={styles.post_headerTime}>
+                            {new Date(comment.timestamp?.toDate()).toLocaleString()}
+                        </span>
+                            </div>
+                        ))}
+
+                        <form onSubmit={newComment}>
+                            <div className={styles.post_form}>
+                                <input
+                                    className={styles.post_input}
+                                    type={"text"}
+                                    placeholder={"Type new comment..."}
+                                    value={comment}
+                                    onChange={(e) => handleChangeComment(e)}
+                                />
+                                <button
+                                    className={comment ? styles.post_button : styles.post_buttonDisable}
+                                    disabled={!comment}
+                                    type={"submit"}
+                                >
+                                    <SendIcon className={styles.post_sendIcon}/>
+                                </button>
+                            </div>
+                        </form>
+                    </>
+                }
             </div>
         </div>
     )
--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -1,12 +1,46 @@
-import { ChangeEvent, FormEvent, useCallback, useState } from "react";
+import { ChangeEvent, FormEvent, useCallback, useEffect, useState } from "react";
 import { db } from "../firebase";
 import { useSelector } from "react-redux";
 import { selectUser } from "../slicers/userSlice";
 import firebase from "firebase/app";
+import { Comment } from "../components/Post";
 
 export const useComment = (postId: string) => {
     const user = useSelector(selectUser)
     const [comment, setComment] = useState("")
+    const [comments, setComments] = useState<Comment[]>([
+        {
+            id: '',
+            avatar: '',
+            text: '',
+            timestamp: null,
+            username: ''
+        }
+    ])
+    const [openComments, setOpenComments] = useState(false)
+
+    const handleChangeOpenComments = () => {
+        setOpenComments(!openComments)
+    }
+
+    useEffect(() => {
+        const unSub = db.collection("posts")
+            .doc(postId)
+            .collection("comments")
+            .orderBy("timestamp", "desc")
+            .onSnapshot((snapshot) => {
+                setComments(snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                    avatar: doc.data().avatar,
+                    text: doc.data().text,
+                    username: doc.data().username,
+                    timestamp: doc.data().timestamp
+                })))
+            })
+        return () => {
+            unSub()
+        }
+    }, [postId])
 
     const handleChangeComment = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         setComment(e.target.value)
@@ -27,6 +61,9 @@ export const useComment = (postId: string) => {
     return {
         comment,
         handleChangeComment,
-        newComment
+        newComment,
+        comments,
+        openComments,
+        handleChangeOpenComments
     }
 }
